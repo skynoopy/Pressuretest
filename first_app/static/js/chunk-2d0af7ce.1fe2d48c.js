437294ca85e4d7d58130b3d5017e3247f93d34f3
[shaokai.bai] <[shaokai.bai@wenba100.com]>
2020-11-04 14:17:08 +0800

    change11021418

--- first_app/static/js/chunk-2d0af7ce.1fe2d48c.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0af7ce"],{"0f13":function(n,u,c){"use strict";c.r(u);var e,l,a=c("2877"),o={},p=Object(a["a"])(o,e,l,!1,null,null,null);u["default"]=p.exports}}]);
